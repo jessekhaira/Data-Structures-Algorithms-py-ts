809c1009352b5cf708ea65e5c884b40fe4336e04
13jk59 <jesse.khaira10@gmail.com>
2021-01-12 08:47:46 -0700

    updated documentation

--- py/algorithms/breadthFirstSearch.py
+++ py/algorithms/breadthFirstSearch.py
@@ -1,13 +1,11 @@
 from collections import deque
 def breadthFirstSearch(node):
     """
-    This algorithm contains the breadth first search
-    traversal algorithm used specifically for a binary tree.
+    This algorithm represents the breadth first searc traversal algorithm used specifically for a binary tree.
 
-    For shortest path problems from node i to node j in 
-    unweighted directed or undirected graphs, BFS is the algorithm 
-    to use. This algorithm makes use of the queue data structure, 
-    which is known as a first in first out data structure. 
+    For shortest path problems from node i to node j in unweighted directed or undirected graphs, BFS is the 
+    algorithm to use. This algorithm makes use of the queue data structure, which is known as a first in first 
+    out data structure. 
 
     Input:
         -> node (Binary Tree Root): Root of binary tree to traverse, with properties
