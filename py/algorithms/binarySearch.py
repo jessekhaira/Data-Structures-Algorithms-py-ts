593592b4cd08b21e3649c45cb8f78c0076f0c484
13jk59 <jesse.khaira10@gmail.com>
2021-01-28 10:39:39 -0700

    updated documentation

--- py/algorithms/binarySearch.py
+++ py/algorithms/binarySearch.py
@@ -1,26 +1,24 @@
 def iterativeBinarySearch(array, target):
     """
-    This algorithm represents the binary search algorithm implemented
-    iteratively. This algorithm finds the position of a target value within
-    within a sorted array by dividing a range of values into halves, and continuing 
-    to narrow down the search space until the desired value is found. As such, 
-    it is an example of a divide and conquer algorithm. 
+    This algorithm represents the binary search algorithm implemented iteratively. This algorithm 
+    finds the position of a target value within within a sorted array by dividing a range of values 
+    into halves, and continuing to narrow down the search space until the desired value is found. 
+    As such, it is an example of a divide and conquer algorithm. 
 
-    This algorithm assumes the simplest case of binary search searching over an explicit
-    sorted array, but the search space can be any space that is sorted. For example, 
-    binary search can be used on functions which are monotonically increasing 
-    or decreasing in order to find the lowest possible value some condition is met (low
-    bound binary search), or the highest possible value some condition is met (high bound
-    binary search). 
+    This algorithm assumes the simplest case of binary search searching over an explicit sorted array, 
+    but the search space can be any space that is sorted. For example, binary search can be used on 
+    functions which are monotonically increasing  or decreasing in order to find the lowest possible 
+    value some condition is met (low bound binary search), or the highest possible value some condition 
+    is met (high bound binary search). 
 
-    Key tip: If the search space consists of integers, then test the binary search on a two
-    element set in order to ensure convergence. 
+    Key tip: If the search space consists of integers, then test the binary search on a two element set 
+    in order to ensure convergence. 
 
     Input:
-    - array(list[int]): List of integers sorted in ascending order
-    - target(int): Integer we're looking for in the array 
+        - array(list[int]): List of integers sorted in ascending order
+        - target(int): Integer we're looking for in the array 
     Output:
-    - int: Represents the idx at which the target value occurs, or -1 if it does not occur
+        - Integer representing the idx at which the target value occurs, or -1 if it does not occur
     """
     # Define search space is indices of array
     left = 0
