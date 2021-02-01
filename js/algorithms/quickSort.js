24f4370469623f495aaf5220f2424e8292e5b334
13jk59 <jesse.khaira10@gmail.com>
2021-02-01 09:05:06 -0700

    updated quicksort js documentation

--- js/algorithms/quickSort.js
+++ js/algorithms/quickSort.js
@@ -1,10 +1,22 @@
 
 /**
- * This function represents an efficient implementation of the quick sort
-   algorithm that runs in O(NlogN) time and O(logN) space, where N is the 
-   length of the input array.
- * @param {number[]} array List of integers to sort in ascending
-    order
+ *  This function represents an efficient implementation of the quick sort algorithm. Like the merge sort
+    algorithm, this algorithm uses the divide and conquer strategy to efficiently sort an array. Unlike merge 
+    sort, which splits arrays in half every time, quicksort places a pivot element (chosen to be the leftmost 
+    element in the array in this implementation) to its final sorted position. Quicksort then recurses and sorts 
+    all the elements to the left of a pivot element, and all the elements to the right of a pivot element until
+    the base case is hit.   
+
+ * Time:
+ *- O(NlogN) best/avg
+ *- O(N**2) worst 
+
+ *Space:
+ *- O(logN) best/avg/worst 
+    
+ *N - number of elements in the input array 
+
+ * @param {number[]} array List of integers to sort in ascending order
  * @returns {number[]} List of integers sorted in ascending order 
  */
 function quickSort(array) {
