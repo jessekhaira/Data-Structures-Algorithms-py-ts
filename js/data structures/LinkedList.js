954022295b99faa7300a2262f978227a20f97890
13jk59 <jesse.khaira10@gmail.com>
2021-02-04 11:07:15 -0700

    finished addAtHead and addAtTail methods js

--- js/data structures/LinkedList.js
+++ js/data structures/LinkedList.js
@@ -30,4 +30,30 @@ class DoubleLinkedList {
         }
         return -1; 
     }
+
+    addAtHead(val) {
+        let newNode = new DoubleLinkedListNode(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode; 
+        }
+    }
+
+    addAtTail(val) {
+        let newNode = new DoubleLinkedListNode(val);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+    }
 }
