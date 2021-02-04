e7c913ea0250afb308cadf5e5098d7f748472642
13jk59 <jesse.khaira10@gmail.com>
2021-02-04 11:04:17 -0700

    coded out get method for doubly linked list js

--- js/data structures/LinkedList.js
+++ js/data structures/LinkedList.js
@@ -15,4 +15,19 @@ class DoubleLinkedList {
         this.head = new DoubleLinkedListNode(val);
         this.tail = this.head; 
     }
+
+    get(index) {
+        let currIdx = 0;
+        let node = this.head;
+        while (node && currIdx <= index) {
+            if (currIdx === index) {
+                return node.val;
+            }
+            else {
+                currIdx++;
+                node = node.next; 
+            }
+        }
+        return -1; 
+    }
 }
