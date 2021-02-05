5d67b65a070a538b91fc0344ec9c610dca3df315
13jk59 <jesse.khaira10@gmail.com>
2021-02-05 13:08:10 -0700

    added documentation

--- py/data_structures/HashSet.py
+++ py/data_structures/HashSet.py
@@ -16,7 +16,8 @@ class HashSet:
 
     def add(self, val): 
         """
-        Inserts the input argument val into the hash set
+        Inserts the input argument, expected to be an integer, into the hash set. If by adding the element, the
+        design load factor is exceeded, rehashing is done. 
         """ 
         hash_val = self._hashing_algorithm(val)
         node_wrapper_val = ChainingNode(val)
@@ -24,9 +25,16 @@ class HashSet:
             self._buckets[hash_val] = node_wrapper_val
         else:
             node = self._buckets[hash_val] 
-            while node.next:
-                node = node.next 
-            node.next = node_wrapper_val
+            prev = None 
+            while node:
+                # unique collection of items - if we're adding same item twice, then only one of them counts
+                if node.val == val:
+                    return 
+                else:
+                    prev = node
+                    node = node.next 
+            
+            prev.next = node_wrapper_val
 
         # adding items to hash set increases current load factor, and if the load factor is >= then
         # the design load factor, we rehash to keep the time complexity of the hash set methods low 
@@ -34,7 +42,17 @@ class HashSet:
         curr_load_factor = (self._curr_items_hashed)/len(self._buckets)
         if curr_load_factor >= self._design_load_factor:
             self._rehash()
-    
+
+    def _rehash(self):
+        saved_version_old_buckets = self._buckets
+        self._buckets = (len(self._buckets)*2)*[None]
+        self._curr_items_hashed = 0 
+        for node in saved_version_old_buckets:
+            while node:
+                self.add(node.val)
+                node = node.next 
+
+
     def contains(self, val):
         """
         Returns a boolean indicating whether or not hash set contains val 
@@ -56,12 +74,17 @@ class HashSet:
         prev = None 
         while node:
             if node.val == val:
-                # edge case - deleting from bucket when only one node exists in it 
+                # edge case - deleting head of linked list from bucket
+                # means the new head will be the node after the head node (which can be None)
                 if not prev:
-                    self._buckets[hash_val] = None
+                    self._buckets[hash_val] = node.next 
                 else:
                     savedNext = node.next 
                     prev.next = savedNext
+                return 
+            else:
+                prev = node
+                node = node.next 
     
     def _hashing_algorithm(self, val):
         return val % len(self._buckets)
