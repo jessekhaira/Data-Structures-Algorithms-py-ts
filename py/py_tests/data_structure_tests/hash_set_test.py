39468fe7b89c7ab9d7e68a6668f93c0350f0c9e8
13jk59 <jesse.khaira10@gmail.com>
2021-02-05 13:17:39 -0700

    added tests for hashset

---
+++ py/py_tests/data_structure_tests/hash_set_test.py
@@ -0,0 +1,40 @@
+import unittest
+from py.data_structures.HashSet import HashSet
+
+class tests(unittest.TestCase):
+    def test1(self):
+        obj1 = HashSet()
+        obj1.add(5)
+        obj1.add(9)
+        obj1.add(5)
+
+        self.assertEqual(obj1.contains(5), True)
+        self.assertEqual(obj1.contains(12), False)
+        self.assertEqual(obj1.contains(9), True)
+
+
+    def test2(self):
+        obj1 = HashSet()
+        obj1.add(0)
+        obj1.add(-123)
+        obj1.add(1231)
+        obj1.add(14123)
+
+        self.assertEqual(obj1.contains(14123), True)
+        obj1.remove(14123)
+
+        self.assertEqual(obj1.contains(14123), False)
+
+        obj1.remove(-123)
+        self.assertEqual(obj1.contains(-123), False)
+
+    def test3(self):
+        obj1 = HashSet()
+        for i in range(1000):
+            obj1.add(1000)
+
+        self.assertEqual(obj1._curr_items_hashed, 1)
+
+
+if __name__ == "__main__":
+    unittest.main()
