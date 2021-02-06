cabc715347052ed5cc22c59b151eecfab768e2fb
13jk59 <jesse.khaira10@gmail.com>
2021-02-06 15:01:52 -0700

    finished javascript hash sets with tests

--- js/data structures/HashSet.js
+++ js/data structures/HashSet.js
@@ -1,10 +1,97 @@
+class ChainingNode{
+    constructor(value){
+        this.val = value;
+        this.next = null; 
+    }
+}
+
 /**
  * This class represents a Hash Set designed specifically to accept integer values. Hash collisons are dealt with 
  * through chaining with linked lists. 
 */
 class HashSet{
+    constructor(init_capacity = 1000, load_factor = 0.75){
+        this._buckets = Array(init_capacity).fill(null);
+        this._design_load_factor = load_factor;
+        this._curr_items_hashed = 0;
+    }
 
-    constructor(){
+    add(key) {
+        const hashVal = this._hashing_algorithm(key);
+        const newNode = new ChainingNode(key);
+        if(!this._buckets[hashVal]) {
+            this._buckets[hashVal] = newNode;
+        }
+        else {
+            let node = this._buckets[hashVal];
+            let prev = null; 
+            while (node) {
+                if (node.val === key) {
+                    return;
+                }
+                prev = node;
+                node = node.next;
+            }
+            prev.next = newNode;
+        }
 
+        this._curr_items_hashed++;
+        const currLoadFactor = (this._curr_items_hashed/this._buckets.length);
+        if (currLoadFactor >= this._design_load_factor) {
+            this.rehash();
+        }
+    }
+
+    rehash() {
+        const savedBuckets = this._buckets;
+        this._curr_items_hashed = 0; 
+        this._buckets = Array(savedBuckets.length *2).fill(null); 
+        for (let node of savedBuckets) {
+            while (node) {
+                this.add(node.val);
+                node = node.next; 
+            }
+        }
+    }
+
+    remove(key) {
+        const hashVal = this._hashing_algorithm(key);
+        let node = this._buckets[hashVal];
+        let prev = null; 
+        while (node) {
+            if (node.val === key) {
+                if (!prev) {
+                    this._buckets[hashVal] = node.next; 
+                }
+                else {
+                    prev.next = node.next; 
+                }
+                return;
+            }
+            else {
+                prev = node; 
+                node = node.next; 
+            }
+        }
+    }
+
+    contains(key) {
+        const hashVal = this._hashing_algorithm(key);
+        let node = this._buckets[hashVal];
+        while (node) {
+            if (node.val === key) {
+                return true; 
+            }
+            else {
+                node = node.next; 
+            }
+        }
+        return false; 
+    }
+
+    _hashing_algorithm(key) {
+        return key%this._buckets.length; 
     }
 }
+
+export {HashSet}; 
---
+++ js/tests/data_structure_tests/hash_set.test.js
@@ -0,0 +1,44 @@
+import {HashSet} from "../../data structures/HashSet.js";
+
+
+test('test1', () => {
+    let obj1 = new HashSet();
+    obj1.add(5);
+    obj1.add(9);
+    obj1.add(5);
+
+    expect(obj1.contains(5)).toStrictEqual(true);
+    expect(obj1.contains(9)).toStrictEqual(true);
+    expect(obj1.contains(12)).toStrictEqual(false);
+
+});
+
+
+test('test 2', () => {
+    let obj2 = new HashSet();
+    obj2.add(0);
+    obj2.add(-123);
+    obj2.add(-123);
+    obj2.add(1231);
+    obj2.add(14123);
+    
+    expect(obj2.contains(14123)).toStrictEqual(true);
+    obj2.remove(14123);
+
+    expect(obj2.contains(14123)).toStrictEqual(false);
+
+    obj2.remove(-123);
+    expect(obj2.contains(-123)).toStrictEqual(false);
+
+});
+
+test('test 3', () => {
+    let obj2 = new HashSet();
+    for (let i=0; i<1000; i++) {
+        obj2.add(0);
+    }
+
+    expect(obj2._curr_items_hashed).toStrictEqual(1); 
+});
+
+
