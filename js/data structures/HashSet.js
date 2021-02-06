93797d125f46f608ffe0168dab8be16ad554802e
13jk59 <jesse.khaira10@gmail.com>
2021-02-06 13:37:20 -0700

    started hash set class js

---
+++ js/data structures/HashSet.js
@@ -0,0 +1,10 @@
+/**
+ * This class represents a Hash Set designed specifically to accept integer values. Hash collisons are dealt with 
+ * through chaining with linked lists. 
+*/
+class HashSet{
+
+    constructor(){
+
+    }
+}
