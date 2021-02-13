ecd91e1ee4bafea0bc1d64b2a2acf6a537509763
13jk59 <jesse.khaira10@gmail.com>
2021-02-12 17:05:32 -0700

    added code defining nodes in the stack, and added constructor for stacks

--- py/data_structures/Stack.py
+++ py/data_structures/Stack.py
@@ -1,8 +1,13 @@
 class StackNode:
-    def __init__(self):
-        pass
+    def __init__(self, val):
+        self.val = val
+        self.next = None 
 
 class Stack:
-    def __init__(self):
-        pass
+    def __init__(self, val):
+        self.head = StackNode(val)
+        self.tail = self.head 
+    
+    def push(self,val):
+        pass 
 
