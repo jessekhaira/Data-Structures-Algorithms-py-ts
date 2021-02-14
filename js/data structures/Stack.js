7da256ed4b9258aa971a8e1b413edb29b8ea7fd5
13jk59 <jesse.khaira10@gmail.com>
2021-02-14 12:48:11 -0700

    started on js implementation of stacks

---
+++ js/data structures/Stack.js
@@ -0,0 +1,17 @@
+class StackNode{
+    constructor(val) {
+        this.val = val;
+        this.prev = null;
+        this.next = null; 
+    }
+}
+
+class Stack {
+    constructor() {
+        this.tail = null; 
+    }
+
+    push(val) {
+        
+    }
+}
