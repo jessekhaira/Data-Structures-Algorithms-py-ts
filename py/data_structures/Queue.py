a416d8a1b1926fdda2e52c4ef7235b9f29c24a12
13jk59 <jesse.khaira10@gmail.com>
2021-02-15 14:50:22 -0700

    added push method for queues

--- py/data_structures/Queue.py
+++ py/data_structures/Queue.py
@@ -6,4 +6,14 @@ class QueueNode:
 class Queue:
     def __init__(self):
         self.head = None
+        self.tail = None 
     
+    def push(self, val):
+        new_node = QueueNode(val)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+            
