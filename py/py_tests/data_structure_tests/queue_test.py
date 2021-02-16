9be5c1d6be2c51b5d5e7cbfbd0dfa3efbd095aee
13jk59 <jesse.khaira10@gmail.com>
2021-02-16 14:40:18 -0700

    added py tests

--- py/py_tests/data_structure_tests/queue_test.py
+++ py/py_tests/data_structure_tests/queue_test.py
@@ -9,7 +9,15 @@ class queue(unittest.TestCase):
         self.assertRaises(IndexError, queue.top) 
 
     def test2(self):
-        pass 
+        queue = Queue()
+        for i in range(150):
+            queue.push(i)
+        
+        for i in range(150):
+            self.assertEqual(len(queue), 150-i)
+            self.assertEqual(queue.top(), i)
+            self.assertEqual(queue.poll(), i)
+
     
 
 if __name__ == "__main__":
