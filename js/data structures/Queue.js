eba99d4436bcd99ed870a7e34ed2efb4ac46e1f4
13jk59 <jesse.khaira10@gmail.com>
2021-02-16 14:42:07 -0700

    added queue js

---
+++ js/data structures/Queue.js
@@ -0,0 +1,13 @@
+class QueueNode {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+}
