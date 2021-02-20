c3ab087f00a3748cd9c4b40dcea184dda2d4fc7f
13jk59 <jesse.khaira10@gmail.com>
2021-02-19 17:29:41 -0700

    added Deque class and method outlines

---
+++ py/data_structures/Deque.py
@@ -0,0 +1,21 @@
+class DequeNode:
+    def __init__(self, val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class Deque:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def peek_first(self):
+        pass
+    
+    def peek_last(self):
+        pass
+
+    def add_first(self):
+        pass
+    
