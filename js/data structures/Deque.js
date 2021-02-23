e631788811f74bd464fd0fe1c7e1dbaa3fd2806e
13jk59 <jesse.khaira10@gmail.com>
2021-02-22 17:03:59 -0700

    added Deque for js

---
+++ js/data structures/Deque.js
@@ -0,0 +1,14 @@
+class DequeNode() {
+    construtor(val) {
+        this.val = val;
+        this.next = null;
+        this.prev = null; 
+    }
+}
+
+class Deque {
+    constructor() {
+        this.head = null;
+        this.tail = null; 
+    }
+}
