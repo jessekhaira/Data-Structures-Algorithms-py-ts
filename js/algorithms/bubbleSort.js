a63f64f3e65f486d3d88e26d22fb6e0a6205f9f2
13jk59 <jesse.khaira10@gmail.com>
2021-02-26 10:04:01 -0700

    finished js bubble sort algorithm

--- js/algorithms/bubbleSort.js
+++ js/algorithms/bubbleSort.js
@@ -13,5 +13,24 @@
  * @returns {number[]} Array of integers sorted in ascending order 
  */
 function bubblesort(array) {
-    
+    let end_idx = array.length;
+    let curr_idx = 0;
+    while (end_idx > 0) {
+        // bubble up largest element in array every time 
+        while (curr_idx < end_idx-1) {
+            let curr_val = array[curr_idx];
+            let next_val = array[curr_idx+1];
+            if (curr_val >= next_val) {
+                swap(array, curr_idx, curr_idx+1)
+            }
+            curr_idx++;
+        }
+        curr_idx = 0; 
+        end_idx--;
+    }
+    return array; 
+}
+
+function swap(array, i, j) {
+    [array[i], array[j]] = [array[j], array[i]]; 
 }
