2f2ee9821fdfcc4f9c78ead48316e420c80e0fed
13jk59 <jesse.khaira10@gmail.com>
2021-02-26 10:07:26 -0700

    changed bfs implementation to use my queue data structure

--- py/algorithms/breadthFirstSearch.py
+++ py/algorithms/breadthFirstSearch.py
@@ -1,33 +1,34 @@
-from collections import deque
+from py.data_structures.Queue import Queue
 def breadthFirstSearch(node):
     """
-    This algorithm represents the breadth first searc traversal algorithm used specifically for a binary tree.
+    This algorithm represents the breadth first search traversal algorithm used specifically for a 
+    binary tree.
 
-    For shortest path problems from node i to node j in unweighted directed or undirected graphs, BFS is the 
-    algorithm to use. This algorithm makes use of the queue data structure, which is known as a first in first 
-    out data structure. 
+    For shortest path problems from node i to node j in unweighted directed or undirected graphs, BFS 
+    is the algorithm to use. This algorithm makes use of the queue data structure, which is known as a 
+    first in first out data structure. 
 
     Input:
         - node (Binary Tree Root): Root of binary tree to traverse, with properties
         .val to represent the value held at this node, .left to represent the left subtree
         attached to this node, .right to represent the right subtree attached to this node 
+    
     Output:
-        - list(list[int]) of binary tree levels
-
+        A list(list[int]) of binary tree levels
     """
-    queue = deque()
-    queue.append(node)
+    queue = Queue() 
+    queue.push(node) 
     output = []
     while queue:
         currLevel = []
         currLength = len(queue)
         for i in range(currLength):
-            node = queue.popleft()
+            node = queue.poll()
             currLevel.append(node.val)
             if node.left:
-                queue.append(node.left)
+                queue.push(node.left)
             if node.right:
-                queue.append(node.right)
+                queue.push(node.right)
         output.append(currLevel)
     return output 
 
