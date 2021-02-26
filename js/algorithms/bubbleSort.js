a47e83a99097df483a16b586aed8f6d7aa90378d
13jk59 <jesse.khaira10@gmail.com>
2021-02-26 09:52:10 -0700

    added bubble sort js

---
+++ js/algorithms/bubbleSort.js
@@ -0,0 +1,17 @@
+/**
+ * This algorithm represents the bubble sort algorithm, meant to be used to sort an
+    array of integers in ascending order.
+
+ *Time:
+ *- O(N**2) best/avg/worst
+    
+ *Space:
+ *- O(1) best/avg/worst
+
+ * N - number of elements in input array
+ * @param {number[]} array Array of integers
+ * @returns {number[]} Array of integers sorted in ascending order 
+ */
+function bubblesort(array) {
+    
+}
