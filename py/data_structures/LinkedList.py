4f5c478b6bd7e4187f02b4c5e39b67ae6f62ad43
13jk59 <jesse.khaira10@gmail.com>
2021-03-13 12:34:51 -0700

    refactored code, wrote tests

--- py/data_structures/LinkedList.py
+++ py/data_structures/LinkedList.py
@@ -1,14 +1,4 @@
-class DoublyLinkedListNode:
-    """
-    This class represents a node meant to be used within doubly linked lists.
-    """ 
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-        self.prev = None 
-
-    
-
+from py.data_structures.utils.LinkedList import DoublyLinkedListNode 
 class DoublyLinkedList:
     """
     This class represents a doubly linked list. 
