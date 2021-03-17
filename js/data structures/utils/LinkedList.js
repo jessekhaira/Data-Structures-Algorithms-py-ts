e63cc6015de1109f367890aa0ee2bea12611a80d
13jk59 <jesse.khaira10@gmail.com>
2021-03-17 09:56:58 -0600

    added documentation

--- js/data structures/utils/LinkedList.js
+++ js/data structures/utils/LinkedList.js
@@ -1,3 +1,8 @@
+/**
+ * This class represents a doubly linked node, where every node has two pointers. Meant to be used within
+ * a doubly linked list. 
+ * @class @public 
+ */
 class DoubleLinkedListNode {
     constructor(val) {
         this.val = val;
@@ -5,7 +10,11 @@ class DoubleLinkedListNode {
         this.next = null;
     }
 }
-
+/**
+ * This class represents a singly linked node, where every node has one pointer. Meant to be used within a
+ * singly linked list. 
+ * @class @public 
+ */
  class SingleLinkedListNode {
     constructor(val) {
         this.val = val;
