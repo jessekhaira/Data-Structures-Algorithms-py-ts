0d1bf4c6bfd11c69b06acafb3a9678e0a21b30f4
13jk59 <jesse.khaira10@gmail.com>
2021-03-20 10:31:11 -0600

    added last test for floyds algo js

--- js/tests/algorithm_tests/FloydsTortoiseHare.test.js
+++ js/tests/algorithm_tests/FloydsTortoiseHare.test.js
@@ -18,7 +18,7 @@ test('test linked list with no cycle', () => {
     expect(floydsTortoiseHareAlgo(node)).toEqual(null); 
 });
 
-test('test linked list with a cycle', () => {
+test('test linked list with a cycle 1', () => {
     const node = new SingleLinkedListNode(2);
     node.next = new SingleLinkedListNode(5);
     node.next.next = new SingleLinkedListNode(9);
@@ -27,3 +27,14 @@ test('test linked list with a cycle', () => {
 
     expect(floydsTortoiseHareAlgo(node)).toEqual(node);
 })
+
+test('test linked list with a cycle 2', () => {
+    const node = new SingleLinkedListNode(2);
+    node.next = new SingleLinkedListNode(5);
+    node.next.next = new SingleLinkedListNode(9);
+    node.next.next.next = new SingleLinkedListNode(12);
+    node.next.next.next.next = new SingleLinkedListNode(5);
+    node.next.next.next.next.next = node.next.next.next.next;
+    expect(floydsTortoiseHareAlgo(node)).toEqual(node.next.next.next.next);
+
+}); 
