4d11c2fe7527e51b97acae14212ac90ed8707715
13jk59 <jesse.khaira10@gmail.com>
2021-03-20 10:28:40 -0600

    added more tests for floyds algo for js

--- js/tests/algorithm_tests/FloydsTortoiseHare.test.js
+++ js/tests/algorithm_tests/FloydsTortoiseHare.test.js
@@ -1,6 +1,19 @@
 import {floydsTortoiseHareAlgo} from '../../algorithms/FloydsTortoiseHareAlgo.js';
+import {SingleLinkedListNode} from '../../data structures/utils/LinkedList';
 
 test('test edge case -- no input', () => {
     expect(floydsTortoiseHareAlgo()).toEqual(null)
 });
 
+test('test edge case -- single node passed in', () => {
+    const node = new SingleLinkedListNode(5);
+    expect(floydsTortoiseHareAlgo(node)).toEqual(null); 
+});
+
+test('test linked list with no cycle', () => {
+    const node = new SingleLinkedListNode(2);
+    node.next = new SingleLinkedListNode(5);
+    node.next.next = new SingleLinkedListNode(9);
+    node.next.next.next = new SingleLinkedListNode(12);
+    expect(floydsTortoiseHareAlgo(node)).toEqual(null); 
+})
