b45580ca75771fb1f30d5ff764005c66af4891a5
13jk59 <jesse.khaira10@gmail.com>
2021-04-10 12:34:06 -0600

    added googles pylintrc for code style

--- py/algorithms/breadthFirstSearch.py
+++ py/algorithms/breadthFirstSearch.py
@@ -1,12 +1,11 @@
 from py.data_structures.Queue import Queue
 def breadthFirstSearch(node):
-    """
-    This algorithm represents the breadth first search traversal algorithm used specifically for a 
-    binary tree.
+    """This algorithm represents the breadth first search traversal algorithm used specifically for
+    a binary tree.
 
     For shortest path problems from node i to node j in unweighted directed or undirected graphs, BFS 
     is the algorithm to use. This algorithm makes use of the queue data structure, which is known as a 
-    first in first out data structure. 
+    first in first out data structure.
 
     Input:
         - node (Binary Tree Root): Root of binary tree to traverse, with properties
@@ -16,8 +15,8 @@ def breadthFirstSearch(node):
     Output:
         A list(list[int]) of binary tree levels
     """
-    queue = Queue() 
-    queue.push(node) 
+    queue = Queue()
+    queue.push(node)
     output = []
     while queue:
         currLevel = []
@@ -31,6 +30,3 @@ def breadthFirstSearch(node):
                 queue.push(node.right)
         output.append(currLevel)
     return output 
-
-
-
