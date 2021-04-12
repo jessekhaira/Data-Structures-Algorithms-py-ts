9e38af40fc354df9954016195ef7bddc5cbdcb24
13jk59 <jesse.khaira10@gmail.com>
2021-04-12 10:39:26 -0600

    changed style of code w/ linter and formatter

--- py/algorithms/binary_search.py
+++ py/algorithms/binary_search.py
@@ -1,26 +1,32 @@
-def iterativeBinarySearch(array, target):
-    """
-    This algorithm represents the binary search algorithm implemented iteratively. This algorithm 
-    finds the position of a target value within within a sorted array by dividing a range of values 
-    into halves, and continuing to narrow down the search space until the desired value is found. 
-    As such, it is an example of a divide and conquer algorithm. 
+def iterative_binary_search(array: list[int], target: int) -> int:
+    """This algorithm represents the binary search algorithm implemented
+    iteratively.
 
-    This algorithm assumes the simplest case of binary search searching over an explicit sorted array, 
-    but the search space can be any space that is sorted. For example, binary search can be used on 
-    functions which are monotonically increasing  or decreasing in order to find the lowest possible 
-    value some condition is met (low bound binary search), or the highest possible value some condition 
-    is met (high bound binary search). This means binary searching over the inputs of these functions to
-    determine which input produces the most optimal value -- highest or lowest output value that meets
-    some condition. 
+    This algorithm finds the position of a target value within within a sorted
+    array by dividing a range of values into halves, and continuing to narrow
+    down the search space until the desired value is found. As such, it is an
+    example of a divide and conquer algorithm.
 
-    Key tip: If the search space consists of integers, then test the binary search on a two element set 
-    in order to ensure convergence. 
+    This algorithm assumes the simplest case of binary search searching over an
+    explicit sorted array, but the search space can be any space that is sorted.
+    For example, binary search can be used on functions which are monotonically
+    increasing  or decreasing in order to find the lowest possible value some
+    condition is met (low bound binary search), or the highest possible value
+    some condition is met (high bound binary search). This means binary
+    searching over the inputs of these functions to determine which input
+    produces the most optimal value -- highest or lowest output value that meets
+    some condition.
 
-    Inputs:
-        - array(list[int]): List of integers sorted in ascending order
-        - target(int): Integer we're looking for in the array 
-    Output:
-        - Integer representing the idx at which the target value occurs, or -1 if it does not occur
+    Args:
+        array:
+            List of integers sorted in ascending order
+
+        target:
+            Integer we're looking for in the array
+
+    Returns:
+        Integer representing the idx at which the target value occurs, or -1 if
+        it does not occur
     """
     left = 0
     right = len(array) - 1
