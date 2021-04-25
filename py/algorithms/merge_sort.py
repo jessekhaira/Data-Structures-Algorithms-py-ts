313a65e4f8e9e645c7f23eea5bcb5f1d43c6f7a2
13jk59 <jesse.khaira10@gmail.com>
2021-04-25 09:30:14 -0600

    refactored merge sort algorithm

--- py/algorithms/merge_sort.py
+++ py/algorithms/merge_sort.py
@@ -1,20 +1,28 @@
-def mergesort(array):
-    """
-    This function represents the merge sort algorithm. Merge
-    sort is an algorithm used to efficiently sort an array of 
-    integers in O(NlogN) time using just O(N) space.
+def merge_sort(array):
+    """This function represents the merge sort algorithm used to
+    sort an array of integers in ascending order.
+    
+    Time:
+        O(N**2)
+    Space:
+        O(N) 
+    
+    Where N is the length of the input array
 
-    Inputs:
-        - array(list[int]): List of integers to sort in ascending order
-    Outputs:
+    Args:
+        array:
+            List of integers to sort in ascending order
+
+    Returns:
         Input list of integers sorted in ascending order
 
     """
     auxArray = array[:]
-    mergesorthelper(array, auxArray, 0, len(array)-1)
+    merge_sort_helper(array, auxArray, 0, len(array) - 1)
     return array
 
-def mergesorthelper(array, auxArray, start, end):
+
+def merge_sort_helper(array, auxArray, start, end):
     """
     This helper function helps to implement mergesort. This function is a recursive function that 
     recurses to the base case of start >= end -> when the pointers point to the same element or 
@@ -33,10 +41,11 @@ def mergesorthelper(array, auxArray, start, end):
     # auxArray isn't even sorted yet, can't be used to sort the main array
     # so we sort auxArray first before trying to sort main array
     # have to recurse to base case so we break array in half and sort each half recursively
-    middle = start + (end-start)//2 
-    mergesorthelper(auxArray, array, start, middle)
-    mergesorthelper(auxArray, array, middle+1, end)
-    _merge(array, auxArray, start, middle, middle+1, end)
+    middle = start + (end - start) // 2
+    merge_sort_helper(auxArray, array, start, middle)
+    merge_sort_helper(auxArray, array, middle + 1, end)
+    _merge(array, auxArray, start, middle, middle + 1, end)
+
 
 def _merge(array, auxArray, lstart, lend, rstart, rend):
     """
@@ -62,12 +71,12 @@ def _merge(array, auxArray, lstart, lend, rstart, rend):
             array[ptrArr] = auxArray[ptr2]
             ptr2 += 1
         ptrArr += 1
-    
+
     while ptr1 <= lend:
         array[ptrArr] = auxArray[ptr1]
         ptr1 += 1
         ptrArr += 1
-    
+
     while ptr2 <= rend:
         array[ptrArr] = auxArray[ptr2]
         ptr2 += 1
