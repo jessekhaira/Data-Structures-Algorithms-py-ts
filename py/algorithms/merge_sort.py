fecdb2fccc328a7259d4d54d115764e1556cf858
13jk59 <jesse.khaira10@gmail.com>
2021-04-25 09:36:47 -0600

    cleaned up merge sort algorithm + merge sort tests

--- py/algorithms/merge_sort.py
+++ py/algorithms/merge_sort.py
@@ -1,12 +1,17 @@
-def merge_sort(array):
-    """This function represents the merge sort algorithm used to
+""" This module contains code representing the merge sort algorithm """
+from typing import List
+
+
+def merge_sort(array: List[int]) -> List[int]:
+    """ This function represents the merge sort algorithm used to
     sort an array of integers in ascending order.
-    
+
     Time:
         O(N**2)
+
     Space:
-        O(N) 
-    
+        O(N)
+
     Where N is the length of the input array
 
     Args:
@@ -17,67 +22,88 @@ def merge_sort(array):
         Input list of integers sorted in ascending order
 
     """
-    auxArray = array[:]
-    merge_sort_helper(array, auxArray, 0, len(array) - 1)
+    aux_array = array[:]
+    merge_sort_helper(array, aux_array, 0, len(array) - 1)
     return array
 
 
-def merge_sort_helper(array, auxArray, start, end):
-    """
-    This helper function helps to implement mergesort. This function is a recursive function that 
-    recurses to the base case of start >= end -> when the pointers point to the same element or 
-    when its an invalid configuration, return.
+def merge_sort_helper(array: List[int], aux_array: List[int], start: int,
+                      end: int) -> None:
+    """ This helper function helps to implement mergesort. This function
+    is a recursive function that recurses to the base case of start >= end
+    -> when the pointers point to the same element, or when its an invalid
+    invalid configuration, return.
 
-    We then partition and sort the main array using the help of the auxArray.
+    We then partition and sort the main array using the help of the aux_array.
 
-    Inputs:
-        - array(list[int]): List of integers to sort from start ptr to end ptr
-        - auxArray(list[int]): Copy of the main array used to help sort the array
-        - start(int): Integer representing start idx to sort from
-        - end(int): Integer representing end idx to sort to
+    Args:
+        array:
+            List of integers to sort from start ptr to end ptr
+
+        aux_array:
+            Copy of the main array used to help sort the array
+
+        start:
+            Integer representing start idx to sort from
+
+        end:
+            Integer representing end idx to sort to
     """
     if start >= end:
         return
-    # auxArray isn't even sorted yet, can't be used to sort the main array
-    # so we sort auxArray first before trying to sort main array
-    # have to recurse to base case so we break array in half and sort each half recursively
+    # aux_array isn't even sorted yet, can't be used to sort the main array
+    # so we sort aux_array first before trying to sort main array
+    # have to recurse to base case so we break array in half and sort each
+    # half recursively
     middle = start + (end - start) // 2
-    merge_sort_helper(auxArray, array, start, middle)
-    merge_sort_helper(auxArray, array, middle + 1, end)
-    _merge(array, auxArray, start, middle, middle + 1, end)
+    merge_sort_helper(aux_array, array, start, middle)
+    merge_sort_helper(aux_array, array, middle + 1, end)
+    _merge(array, aux_array, start, middle, middle + 1, end)
 
 
-def _merge(array, auxArray, lstart, lend, rstart, rend):
-    """
-    This function carries out the merge operation using the two sorted partitions of auxArray
-    from lstart to lend, and rstart to rend, to sort the main array.
+def _merge(array: List[int], aux_array: List[int], lstart: int, lend: int,
+           rstart: int, rend: int) -> None:
+    """ This function carries out the merge operation using the
+    two sorted partitions of aux_array from lstart to lend, and
+    rstart to rend, to sort the main array.
 
-    Inputs:
-        - array(list[int]): List of integers to sort from lstart to rend 
-        - auxArray(list[int]): Copy of the main array used to help sort the array
-        - lstart(int): Integer representing start idx of sorted left portion of auxArray
-        - lend(int): Integer representing end idx of sorted left portion of auxArray
-        - rstart(int): Integer representing start idx of sorted right portion of auxArray
-        - rend(int): Integer representing end idx of sorted right portion of auxArray 
+    Args:
+        array:
+            List of integers to sort from lstart to rend
+
+        aux_array:
+            Copy of the main array used to help sort the array
+
+        lstart:
+            Integer representing start idx of sorted left portion of aux_array
+
+        lend:
+            Integer representing end idx of sorted left portion of aux_array
+
+        rstart:
+            Integer representing start idx of sorted right portion of aux_array
+
+        rend:
+            Integer representing end idx of sorted right portion of aux_array
     """
     ptr1 = lstart
-    ptrArr = lstart
+    ptr_arr = lstart
     ptr2 = rstart
     while ptr1 <= lend and ptr2 <= rend:
-        if auxArray[ptr1] <= auxArray[ptr2]:
-            array[ptrArr] = auxArray[ptr1]
+        if aux_array[ptr1] <= aux_array[ptr2]:
+            array[ptr_arr] = aux_array[ptr1]
             ptr1 += 1
         else:
-            array[ptrArr] = auxArray[ptr2]
+            array[ptr_arr] = aux_array[ptr2]
             ptr2 += 1
-        ptrArr += 1
+        ptr_arr += 1
 
     while ptr1 <= lend:
-        array[ptrArr] = auxArray[ptr1]
+        array[ptr_arr] = aux_array[ptr1]
         ptr1 += 1
-        ptrArr += 1
+        ptr_arr += 1
 
     while ptr2 <= rend:
-        array[ptrArr] = auxArray[ptr2]
+        array[ptr_arr] = aux_array[ptr2]
         ptr2 += 1
-        ptrArr += 1
+        ptr_arr += 1
---
+++ py/py_tests/algorithm_tests/merge_sort_test.py
@@ -0,0 +1,21 @@
+import unittest
+from py.algorithms.merge_sort import merge_sort
+
+
+class tests(unittest.TestCase):
+
+    def test1(self):
+        array = [-9, 2, 1, 2, 3, 4, 5, 6, 9, 18]
+        self.assertEqual([-9, 1, 2, 2, 3, 4, 5, 6, 9, 18], merge_sort(array))
+
+    def test2(self):
+        array = []
+        self.assertEqual([], merge_sort(array))
+
+    def test3(self):
+        array = [-1000, 21, 2, 5, 9, -2000, 21]
+        self.assertEqual([-2000, -1000, 2, 5, 9, 21, 21], merge_sort(array))
+
+
+if __name__ == "__main__":
+    unittest.main()
