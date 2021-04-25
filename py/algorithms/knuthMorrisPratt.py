31131aa86a189f8025f019b22c014e6f6738a27e
13jk59 <jesse.khaira10@gmail.com>
2021-04-25 09:38:30 -0600

    linted kmp algorithm

--- py/algorithms/knuthMorrisPratt.py
+++ py/algorithms/knuthMorrisPratt.py
@@ -20,20 +20,22 @@ def knuthMorrisPratt(bigstr, substr):
     prefixSuffixTable = makePrefixSuffixTable(substr)
     return match(bigstr, substr, prefixSuffixTable)
 
+
 def makePrefixSuffixTable(substr):
     table = [-1] * len(substr)
     i = 0
     j = 1
     while j < len(substr):
         if substr[j] == substr[i]:
-            table[j] = i 
+            table[j] = i
             j += 1
             i += 1
         elif i != 0:
-            i = table[i-1] + 1
+            i = table[i - 1] + 1
         else:
             j += 1
     return table
+
 
 def match(bigstr, substr, prefixSuffixTable):
     i = 0
@@ -43,8 +45,7 @@ def match(bigstr, substr, prefixSuffixTable):
             i += 1
             j += 1
         elif j != 0:
-            j = prefixSuffixTable[j-1] + 1
+            j = prefixSuffixTable[j - 1] + 1
         else:
             i += 1
-    return i-j if j == len(substr) else -1 
-
+    return i - j if j == len(substr) else -1
