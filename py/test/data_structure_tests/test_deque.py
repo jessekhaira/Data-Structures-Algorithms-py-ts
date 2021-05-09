ced1bdc17b34f193ec3c62410e0f0824872f9cc2
13jk59 <jesse.khaira10@gmail.com>
2021-05-09 16:34:03 -0600

    more updating of tests for unittest

--- py/test/data_structure_tests/test_deque.py
+++ py/test/data_structure_tests/test_deque.py
@@ -1,7 +1,9 @@
 import unittest
 from py.data_structures.Deque import Deque
 
+
 class DequeTests(unittest.TestCase):
+
     def test1(self):
         obj1 = Deque()
         self.assertRaises(IndexError, obj1.pop_first)
@@ -15,37 +17,34 @@ class DequeTests(unittest.TestCase):
             obj2.add_first(i)
             self.assertEqual(obj2.peek_first(), i)
             self.assertEqual(obj2.peek_last(), 0)
-    
+
     def test3(self):
         obj3 = Deque()
         for i in range(150):
             obj3.add_last(i)
             self.assertEqual(obj3.peek_first(), 0)
             self.assertEqual(obj3.peek_last(), i)
-            self.assertEqual(len(obj3), i+1) 
-    
+            self.assertEqual(len(obj3), i + 1)
+
     def test4(self):
         obj4 = Deque()
         for i in range(150):
             obj4.add_last(i)
-        
+
         head = 0
         length = 150
         while obj4:
             self.assertEqual(obj4.pop_first(), head)
             head += 1
-            self.assertEqual(len(obj4), length-head)
-        
+            self.assertEqual(len(obj4), length - head)
+
     def test5(self):
         obj5 = Deque()
         obj5.add_first(21)
 
         self.assertEqual(obj5.pop_first(), 21)
-        self.assertRaises(IndexError, obj5.pop_last) 
+        self.assertRaises(IndexError, obj5.pop_last)
 
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
