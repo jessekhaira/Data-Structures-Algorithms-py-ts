0cee38d518399b3fac18e726dfea50ae9dafe649
13jk59 <jesse.khaira10@gmail.com>
2021-05-10 17:43:53 -0600

    more name changes, readme.md updates to make everything work, testing files updated

--- ts/src/tests/algorithm_tests/HeapSort.test.js
+++ ts/src/tests/algorithm_tests/HeapSort.test.js
@@ -1,17 +1,40 @@
-import heapsort from "../../algorithms/HeapSort.js"
+import heapsort from '../../algorithms/heap_sort.js';
 
 test('test 1', () => {
-    let obj1 = [20,4,5,-9,-120]
-    expect(heapsort(obj1)).toStrictEqual([-120,-9,4,5,20]);
+    const obj1 = [20, 4, 5, -9, -120];
+    expect(heapsort(obj1)).toStrictEqual([-120, -9, 4, 5, 20]);
 });
 
-
 test('test 2', () => {
-    let obj1 = [-9,4,5,9,10,10,20,5,8,-30,-90];
-    expect(heapsort(obj1)).toStrictEqual(obj1.sort((a,b) => a-b));
-})
+    const obj1 = [-9, 4, 5, 9, 10, 10, 20, 5, 8, -30, -90];
+    expect(heapsort(obj1)).toStrictEqual(obj1.sort((a, b) => a - b));
+});
 
 test('test 3', () => {
-    let obj1 = [-9,4,5,9,10,10,20,5,8,-30,-90, 20, 0, 0, 0, 3, 4,5, 0, 2,3, 3,3];
-    expect(heapsort(obj1)).toStrictEqual(obj1.sort((a,b) => a-b));
+    const obj1 = [
+        -9,
+        4,
+        5,
+        9,
+        10,
+        10,
+        20,
+        5,
+        8,
+        -30,
+        -90,
+        20,
+        0,
+        0,
+        0,
+        3,
+        4,
+        5,
+        0,
+        2,
+        3,
+        3,
+        3,
+    ];
+    expect(heapsort(obj1)).toStrictEqual(obj1.sort((a, b) => a - b));
 });
--- ts/src/tests/algorithm_tests/quicksort.test.js
+++ ts/src/tests/algorithm_tests/quicksort.test.js
@@ -1,13 +1,11 @@
-import quickSort from "../../algorithms/quickSort";
-
+import quickSort from '../../algorithms/quick_sort';
 
 test('test 1', () => {
-    let obj1 = [20,4,5,-9,-120]
-    expect(quickSort(obj1)).toStrictEqual([-120,-9,4,5,20]);
+    const obj1 = [20, 4, 5, -9, -120];
+    expect(quickSort(obj1)).toStrictEqual([-120, -9, 4, 5, 20]);
 });
 
-
 test('test 2', () => {
-    let obj1 = [-9,4,5,9,10,10,20,5,8,-30,-90];
-    expect(quickSort(obj1)).toStrictEqual(obj1.sort((a,b) => a-b));
-} )
+    const obj1 = [-9, 4, 5, 9, 10, 10, 20, 5, 8, -30, -90];
+    expect(quickSort(obj1)).toStrictEqual(obj1.sort((a, b) => a - b));
+});
