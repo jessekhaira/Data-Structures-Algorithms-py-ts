5db296ff8646c75d4d46c043e3d3bb299ef2bdc1
13jk59 <jesse.khaira10@gmail.com>
2021-05-11 14:11:03 -0600

    added typescript testing file for breadth first search.ts

---
+++ ts/src/tests/algorithm_tests/breadth_first_search.test.ts
@@ -0,0 +1,30 @@
+import { BinarySearchTree } from '../../data structures/BinarySearchTree';
+import breadthFirstSearch from '../../algorithms/breadth_first_search';
+
+test('test 1 for breadth first search algorithm', () => {
+    const bst: BinarySearchTree = new BinarySearchTree(3);
+    bst.insert(9);
+    bst.insert(10);
+
+    const output = [[3], [9], [10]];
+    expect(breadthFirstSearch(bst.root)).toStrictEqual(output);
+});
+
+test('test 2 for breadth first search algorithm', () => {
+    const bst: BinarySearchTree = new BinarySearchTree(3);
+
+    const output = [[3]];
+    expect(breadthFirstSearch(bst.root)).toStrictEqual(output);
+});
+
+test('test 3 for breadth first search algorithm', () => {
+    const bst: BinarySearchTree = new BinarySearchTree(1235);
+    bst.insert(50);
+    bst.insert(95);
+    bst.insert(3500);
+    bst.insert(2400);
+    bst.insert(2350);
+
+    const output = [[1235], [50, 3500], [95, 2400], [2350]];
+    expect(breadthFirstSearch(bst.root)).toStrictEqual(output);
+});
