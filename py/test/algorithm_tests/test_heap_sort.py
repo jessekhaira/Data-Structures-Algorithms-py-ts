2db6915660b520d90a330b4df6ac56fd96a35a68
13jk59 <jesse.khaira10@gmail.com>
2021-05-19 10:11:06 -0600

    updated test heap sort

--- py/test/algorithm_tests/test_heap_sort.py
+++ py/test/algorithm_tests/test_heap_sort.py
@@ -1,20 +1,22 @@
+""" This module contains code testing the heap sort algorithm """
 import unittest
-from py.algorithms.heapsort import heapsort
+from py.algorithms.heap_sort import heap_sort
 
 
-class tests(unittest.TestCase):
+class TestHeapSort(unittest.TestCase):
+    """ This class contains unit tests for the heap sort algorithm """
 
     def test1(self):
         array = [-9, 2, 1, 2, 3, 4, 5, 6, 9, 18]
-        self.assertEqual([-9, 1, 2, 2, 3, 4, 5, 6, 9, 18], heapsort(array))
+        self.assertEqual([-9, 1, 2, 2, 3, 4, 5, 6, 9, 18], heap_sort(array))
 
     def test2(self):
         array = []
-        self.assertEqual([], heapsort(array))
+        self.assertEqual([], heap_sort(array))
 
     def test3(self):
         array = [-1000, 21, 2, 5, 9, -2000, 21]
-        self.assertEqual([-2000, -1000, 2, 5, 9, 21, 21], heapsort(array))
+        self.assertEqual([-2000, -1000, 2, 5, 9, 21, 21], heap_sort(array))
 
 
 if __name__ == "__main__":
