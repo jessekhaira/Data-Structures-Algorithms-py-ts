5e437583320b82d1a94582ef3bb19958bd2d2140
13jk59 <jesse.khaira10@gmail.com>
2021-06-06 17:26:35 -0600

    cleaned up testing file

--- py/test/data_structure_tests/test_disjoint_set.py
+++ py/test/data_structure_tests/test_disjoint_set.py
@@ -1,19 +1,23 @@
+""" This module contains code for the tests for the disjoint set
+class """
 import unittest
-from py.data_structures.DisjointSet import DisjointSet
+from py.data_structures.disjoint_set import DisjointSet
 
 
+class DisjointSetTests(unittest.TestCase):
+    """ This class contains unit tests for the disjoint set
+    data structure """
 
-class tests(unittest.TestCase):
     def test1(self):
         obj1 = DisjointSet(20)
-        self.assertEqual([-1]*20, obj1.forest)
+        self.assertEqual([-1] * 20, obj1.forest)
 
     def test2(self):
         obj1 = DisjointSet(20)
-        obj1.union(4,5)
-        obj1.union(4,9)
-        obj1.union(4,15)
-        obj1.union(4,19)
+        obj1.union(4, 5)
+        obj1.union(4, 9)
+        obj1.union(4, 15)
+        obj1.union(4, 19)
 
         self.assertEqual(4, obj1.find(4))
         self.assertEqual(4, obj1.find(9))
@@ -22,17 +26,15 @@ class tests(unittest.TestCase):
 
     def test3(self):
         obj1 = DisjointSet(20)
-        obj1.union(4,5)
-        obj1.union(4,9)
-        obj1.union(4,15)
-        obj1.union(4,19)
-        obj1.union(1,19)
-        obj1.union(3,19)
+        obj1.union(4, 5)
+        obj1.union(4, 9)
+        obj1.union(4, 15)
+        obj1.union(4, 19)
+        obj1.union(1, 19)
+        obj1.union(3, 19)
 
         self.assertEqual(4, obj1.find(4))
         self.assertEqual(4, obj1.find(3))
-
-
 
 
 if __name__ == "__main__":
