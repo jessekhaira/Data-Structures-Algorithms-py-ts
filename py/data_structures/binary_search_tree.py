6cbf83421ce73b653773444942ac540cf8f425f4
13jk59 <jesse.khaira10@gmail.com>
2021-06-05 19:12:23 -0600

    finished cleaning up binary search tree module

--- py/data_structures/binary_search_tree.py
+++ py/data_structures/binary_search_tree.py
@@ -1,5 +1,5 @@
 from py.data_structures.utils.binaryTree import binaryTreeNode
-from typing import Union
+from typing import Union, Tuple
 
 
 class BinarySearchTree:
@@ -9,9 +9,9 @@ class BinarySearchTree:
     in the right subtree of that node are greater than that nodes value.
 
     This allows for efficient lookups, insertions, and deletions in the average
-    case of O(logN) T | O(1) S. But this data structure does not guarantee
-    balancing, meaning that in the worst case, there are O(N) T | O(1) S
-    lookups, insertions, and deletions.
+    case of O(logN) time. But this data structure does not guarantee
+    balancing, meaning that in the worst case, lookups, insertions, and
+    deletions will take O(N) time and space.
 
     Attributes:
         val:
@@ -21,7 +21,7 @@ class BinarySearchTree:
     def __init__(self, val: int):
         self.root = binaryTreeNode(val)
 
-    def insertion(self, val: int):
+    def insertion(self, val: int) -> None:
         """ This method inserts a node into the binary search tree, while
         ensuring that the binary search tree property is adhered to.
 
@@ -30,7 +30,8 @@ class BinarySearchTree:
             O(N) worst
 
         Space:
-            O(1) best/avg/worst
+            O(logN) best/average
+            O(N) worst
 
         Where N is the number of nodes contained in the tree
 
@@ -42,7 +43,7 @@ class BinarySearchTree:
         node = self.root
         return self._insertion_helper(val, node)
 
-    def _insertion_helper(self, val: int, node: binaryTreeNode):
+    def _insertion_helper(self, val: int, node: binaryTreeNode) -> None:
         if not node:
             return binaryTreeNode(val)
         elif val < node.val:
@@ -88,26 +89,44 @@ class BinarySearchTree:
         else:
             return self._lookup_helper(node.right, val)
 
-    def delete(self, val):
+    def delete(self, val: int) -> None:
+        """ This method deletes the first node with the given value inside
+        of the binary search tree if it exists.
+
+        Time:
+            O(logN) best/average
+            O(N) worst
+
+        Space:
+            O(logN) best/average
+            O(N) worst
+
+        Where N is the number of nodes in the binary search tree
+
+        Args:
+            val:
+                Integer representing the node value to delete inside of the tree
+        """
         node = self.root
-        self._deleteHelperV(node, val)
+        self._delete_helper_v(node, val)
 
-    def _deleteHelperV(self, node, val):
+    def _delete_helper_v(self, node: binaryTreeNode, val: int):
+        """ This delete helper uses a convenient method to erase
+        the node when the node has two children. This node actually
+        doesn't erase the node with the target value, this method
+        just replaces its value with the minimum value in the nodes
+        right subtree.
+
+        If you want to actually remove the node when the node has two
+        children, use deleteHelperA.
         """
-        This delete helper uses a convenient method to erase the node when the node has two
-        children. This node actually doesn't erase the node with the target value, this method
-        just replaces its value with the minimum value in the nodes right subtree.
-
-        If you want to actually remove the node when the node has two children, use deleteHelperA. 
-        """
-
         if not node:
             return
         elif val < node.val:
-            node.left = self._deleteHelperV(node.left, val)
+            node.left = self._delete_helper_v(node.left, val)
             return node
         elif val > node.val:
-            node.right = self._deleteHelperV(node.right, val)
+            node.right = self._delete_helper_v(node.right, val)
             return node
         else:
             # no left or right child, just delete
@@ -120,32 +139,35 @@ class BinarySearchTree:
             elif not node.right:
                 return node.left
 
-            # if node has two children, replace this nodes value with the minimum value in the
-            # nodes right subtree, and then delete that node
-            minVal_inRightSubtree = self._getMinValRightV(node.right)
-            node.val = minVal_inRightSubtree
-            node.right = self._deleteHelperV(node.right, minVal_inRightSubtree)
+            # if node has two children, replace this nodes value
+            # with the minimum value in the nodes right subtree,
+            # and then delete that node
+            min_val_in_right_subtree = self._get_min_val_right_v(node.right)
+            node.val = min_val_in_right_subtree
+            node.right = self._delete_helper_v(node.right,
+                                               min_val_in_right_subtree)
             return node
 
-    def _getMinValRightV(self, node):
+    def _get_min_val_right_v(self, node: binaryTreeNode) -> int:
         # min val has no left child
         if not node.left:
             return node.val
         return self._getMinValRight(node.left)
 
-    def _deleteHelperA(self, node, val):
-        """
-        This delete helper is the same as the other one, except when the node has two children, this method
-        actually removes the node from the tree rather than just replacing its value with the minimum value
-        in the right subtree. 
+    def _delete_helper_a(self, node: binaryTreeNode,
+                         val: int) -> Union[binaryTreeNode, None]:
+        """ This delete helper is the same as the other one, except when
+        the node has two children, this method actually removes the node
+        from the tree rather than just replacing its value with the minimum
+        value in the right subtree.
         """
         if not node:
             return
         elif val < node.val:
-            node.left = self._deleteHelperA(node.left, val)
+            node.left = self._delete_helper_a(node.left, val)
             return node
         elif val > node.val:
-            node.right = self._deleteHelperA(node.right, val)
+            node.right = self._delete_helper_a(node.right, val)
             return node
         else:
             if not node.left and not node.right:
@@ -157,14 +179,17 @@ class BinarySearchTree:
 
             # have to delete the min val in right subtree while going down
             # so this nodes right subtree has to be changed
-            node.right, minNode = self._getMinNode(node.right)
-            # we have the minNode, set its left and right subtrees and return up
-            minNode.left = node.left
-            minNode.right = node.right
-            return minNode
+            node.right, min_node = self._get_min_node(node.right)
+            # we have the min_node, set its left and right subtrees and
+            # return up
+            min_node.left = node.left
+            min_node.right = node.right
+            return min_node
 
-    def _getMinNode(self, node):
+    def _get_min_node(
+            self,
+            node: binaryTreeNode) -> Tuple[binaryTreeNode, binaryTreeNode]:
         if not node.left:
             return node.right, node
-        node.left, minNode = self._getMinNode(node.left)
-        return node, minNode
+        node.left, min_node = self._get_min_node(node.left)
+        return node, min_node
