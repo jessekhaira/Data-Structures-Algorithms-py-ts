16057ec4f9c7d5429420d9d93d0379ebb1c91ac7
13jk59 <jesse.khaira10@gmail.com>
2021-06-15 14:07:59 -0600

    added documentation for stack data structure

--- py/data_structures/stack.py
+++ py/data_structures/stack.py
@@ -1,7 +1,23 @@
+""" This module contains code for a class that represents the
+stack data structure """
 from py.utils.linked_list import DoublyLinkedListNode
 
 
 class Stack:
+    """ This class represents the stack data structure, built using
+    pythons doubly linked list nodes. The basic principle of the stack is
+    Last In First Out (LIFO), where the last element added will be
+    the first one removed (like a stack of plates).
+
+    Stacks are very useful in cases where you want to process a nested
+    data structure, or for functions that call other functions, even
+    themselves. It follows that common use cases for stacks are for
+    implementing backtracking algorithms, parsers, and so on.
+
+    Attributes:
+        tail:
+            The doubly linked list node on top of the stack
+    """
 
     def __init__(self):
         self.tail = None
