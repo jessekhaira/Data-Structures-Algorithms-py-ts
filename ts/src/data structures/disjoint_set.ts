8eee9ddbb74641649710a80996b6dc130e897d54
13jk59 <jesse.khaira10@gmail.com>
2021-06-18 19:16:34 -0600

    migrated disjoint set to typescript, changed testing module

--- ts/src/data structures/disjoint_set.ts
+++ ts/src/data structures/disjoint_set.ts
@@ -1,31 +1,30 @@
 /**
  *  This class represents an efficient implementation of the DisjointSet data structure.
- *  Not only is the data structure implemented as an array, path compression is used in the 
- *  find() method, and union by rank is used in the union() method. 
- * 
+ *  Not only is the data structure implemented as an array, path compression is used in the
+ *  find() method, and union by rank is used in the union() method.
+ *
  *  This data structure tracks a set of elements partitioned into a number of disjoint subsets,
- *  typically used when dealing with undirected graphs. For example, to determine the number of 
+ *  typically used when dealing with undirected graphs. For example, to determine the number of
  *  connected components within an undirected graph, to determine if a cycle exists in an undirected
- *  graph, etc. 
- *  @public @class 
+ *  graph, etc.
+ *  @public @class
  */
 class DisjointSet {
     /**
-     * 
+     *
      * @param {number} numNodes Int representing how many nodes there are in the undirected graph
      * @param {(object|null)} mapping Mapping between the nodes and indices in the disjoint set
      */
     constructor(numNodes, mapping = null) {
         /**
          * Disjoint Set represented as an array
-         * 
+         *
          * @public
          * @type {number[]}
          */
         this.disjointSet = Array(numNodes).fill(-1);
 
-        
-        this.mapping = mapping; 
+        this.mapping = mapping;
     }
 
     /**
@@ -43,16 +42,16 @@ class DisjointSet {
      * @returns {number} Int representing the representative of the set 
      */
     find(x) {
-        x = (this.mapping === null ? x: this.mapping[x]);
+        x = this.mapping === null ? x : this.mapping[x];
         // if the current node is just pointing at itself, that means its the representative of the set
         if (this.disjointSet[x] <= -1) {
-            return x; 
+            return x;
         }
         // otherwise find the representative of the set and set this nodes pointer to it
-        else {
-            this.disjointSet[x] = this.find(this.disjointSet[x]);
-        }
-        return this.disjointSet[x]; 
+
+        this.disjointSet[x] = this.find(this.disjointSet[x]);
+
+        return this.disjointSet[x];
     }
 
     /**
@@ -74,23 +73,20 @@ class DisjointSet {
      * @returns {null} None 
      */
     union(x, y) {
-        let rX = this.find(x);
-        let rY = this.find(y);
+        const rX = this.find(x);
+        const rY = this.find(y);
         if (rX === rY) {
             return null;
         }
-        else if (this.disjointSet[rX] < this.disjointSet[rY]) {
+        if (this.disjointSet[rX] < this.disjointSet[rY]) {
             this.disjointSet[rY] = rX;
-        }
-        else if (this.disjointSet[rY] < this.disjointSet[rX]) {
-            this.disjointSet[rX] = rY; 
-        }
-
-        else {
+        } else if (this.disjointSet[rY] < this.disjointSet[rX]) {
+            this.disjointSet[rX] = rY;
+        } else {
             this.disjointSet[rY] = rX;
-            this.disjointSet[rX] -= 1; 
+            this.disjointSet[rX] -= 1;
         }
     }
 }
 
-export {DisjointSet}; 
+export { DisjointSet };
---
+++ ts/src/tests/data_structure_tests/disjoint_set.test.ts
@@ -0,0 +1,34 @@
+import { DisjointSet } from '../../data structures/disjoint_set';
+
+test('test 1', () => {
+    const obj1 = new DisjointSet(20);
+    expect(obj1.disjointSet).toStrictEqual(Array(20).fill(-1));
+});
+
+test('test2', () => {
+    const obj1 = new DisjointSet(20);
+    obj1.union(4, 5);
+    obj1.union(4, 9);
+    obj1.union(4, 15);
+    obj1.union(4, 19);
+
+    expect(4).toBe(obj1.find(4));
+    expect(4).toBe(obj1.find(9));
+    expect(4).toBe(obj1.find(15));
+    expect(4).toBe(obj1.find(19));
+});
+
+test('test3', () => {
+    const obj1 = new DisjointSet(20);
+    obj1.union(1, 5);
+    obj1.union(1, 9);
+    obj1.union(4, 15);
+    obj1.union(4, 3);
+    obj1.union(4, 16);
+    obj1.union(3, 1);
+
+    expect(4).toBe(obj1.find(4));
+    expect(4).toBe(obj1.find(3));
+    expect(4).toBe(obj1.find(1));
+    expect(1).not.toBe(obj1.find(9));
+});
