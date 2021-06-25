ef2e41f7bd6f719493d0d36ef7cda018fe37aa7c
13jk59 <jesse.khaira10@gmail.com>
2021-06-25 13:37:45 -0600

    more cleaning and refactoring

--- ts/src/data structures/deque.ts
+++ ts/src/data structures/deque.ts
@@ -34,11 +34,11 @@ class Deque<T> {
     popFirst(): T {
         if (this.head) {
             const retvalue = this.head.val;
-            const new_head = this.head.next;
-            if (new_head) {
-                new_head.prev = null;
+            const newHead = this.head.next;
+            if (newHead) {
+                newHead.prev = null;
                 this.head.next = null;
-                this.head = new_head;
+                this.head = newHead;
             } else {
                 this.head = null;
                 this.tail = null;
@@ -68,26 +68,26 @@ class Deque<T> {
     }
 
     addFirst(value: T): void {
-        const new_head = new DoubleLinkedListNode(value);
+        const newHead = new DoubleLinkedListNode(value);
         if (this.head) {
-            new_head.next = this.head;
-            this.head.prev = new_head;
-            this.head = new_head;
+            newHead.next = this.head;
+            this.head.prev = newHead;
+            this.head = newHead;
         } else {
-            this.head = new_head;
-            this.tail = new_head;
+            this.head = newHead;
+            this.tail = newHead;
         }
     }
 
     addLast(value: T): void {
-        const new_tail = new DoubleLinkedListNode(value);
+        const newTail = new DoubleLinkedListNode(value);
         if (this.tail) {
-            new_tail.prev = this.tail;
-            this.tail.next = new_tail;
-            this.tail = new_tail;
+            newTail.prev = this.tail;
+            this.tail.next = newTail;
+            this.tail = newTail;
         } else {
-            this.head = new_tail;
-            this.tail = new_tail;
+            this.head = newTail;
+            this.tail = newTail;
         }
     }
 
@@ -102,4 +102,4 @@ class Deque<T> {
     }
 }
 
-export { Deque };
+export default Deque;
