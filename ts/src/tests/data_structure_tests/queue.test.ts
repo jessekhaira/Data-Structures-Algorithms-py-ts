1453c646ff7899e5f982106dd30a826b8a82130f
13jk59 <jesse.khaira10@gmail.com>
2021-06-28 10:07:05 -0600

    new test for queues with strings

--- ts/src/tests/data_structure_tests/queue.test.ts
+++ ts/src/tests/data_structure_tests/queue.test.ts
@@ -1,13 +1,13 @@
 import Queue from '../../data structures/queue';
 
 test('test1', () => {
-    const testObj = new Queue();
+    const testObj = new Queue<number>();
     expect(testObj.top).toThrow();
     expect(testObj.pop).toThrow();
 });
 
 test('test2', () => {
-    const testObj = new Queue();
+    const testObj = new Queue<number>();
     for (let i = 0; i < 150; i += 1) {
         testObj.push(i);
     }
@@ -19,4 +19,17 @@ test('test2', () => {
     }
 
     expect(testObj.top).toThrow();
+});
+
+test('test3', () => {
+    const testObj = new Queue<string>();
+    for (let i = 0; i < 100; i += 1) {
+        testObj.push(`a${'a'.repeat(i)}`);
+        expect(testObj.top()).toEqual(`a`);
+        expect(testObj.length()).toEqual(i + 1);
+    }
+
+    for (let i = 0; i < 100; i += 1) {
+        expect(testObj.pop()).toEqual(`a${'a'.repeat(i)}`);
+    }
 });
