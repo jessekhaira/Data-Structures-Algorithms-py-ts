403949f83d7accc853bea0c95d7489d8573964d6
13jk59 <jesse.khaira10@gmail.com>
2021-07-11 13:17:59 -0600

    migrated quicksort to typescript

--- ts/src/algorithms/quick_sort.ts
+++ ts/src/algorithms/quick_sort.ts
@@ -1,3 +1,35 @@
+import { swap } from '../utils/general_utility_functions';
+
+function quickSortHelper(array: number[], start: number, end: number): void {
+    if (start >= end) {
+        return;
+    }
+    const pivot = start;
+    let ptrL = start + 1;
+    let ptrR = end;
+
+    while (ptrL <= ptrR) {
+        if (array[ptrL] > array[pivot] && array[pivot] > array[ptrR]) {
+            swap(array, ptrL, ptrR);
+            ptrL += 1;
+            ptrR -= 1;
+        } else if (array[ptrL] <= array[pivot]) {
+            ptrL += 1;
+        } else if (array[ptrR] >= array[pivot]) {
+            ptrR -= 1;
+        }
+    }
+
+    swap(array, pivot, ptrR);
+
+    if (end - (ptrR + 1) > ptrR - 1 - start) {
+        quickSortHelper(array, start, ptrR - 1);
+        quickSortHelper(array, ptrR + 1, end);
+    } else {
+        quickSortHelper(array, ptrR + 1, end);
+        quickSortHelper(array, start, ptrR - 1);
+    }
+}
 /**
  *  This function represents an efficient implementation of the quick
  *  sort algorithm. Like the merge sort algorithm, this algorithm uses
@@ -21,46 +53,8 @@
  * @returns {number[]} List of integers sorted in ascending order 
  */
 function quickSort(array: number[]): number[] {
-    if (array == null) {
-        return null;
-    }
     quickSortHelper(array, 0, array.length - 1);
     return array;
-}
-
-function quickSortHelper(array, start, end) {
-    if (start >= end) {
-        return;
-    }
-    const pivot = start;
-    let ptrL = start + 1;
-    let ptrR = end;
-
-    while (ptrL <= ptrR) {
-        if (array[ptrL] > array[pivot] && array[pivot] > array[ptrR]) {
-            swap(array, ptrL, ptrR);
-            ptrL++;
-            ptrR--;
-        } else if (array[ptrL] <= array[pivot]) {
-            ptrL++;
-        } else if (array[ptrR] >= array[pivot]) {
-            ptrR--;
-        }
-    }
-
-    swap(array, pivot, ptrR);
-
-    if (end - (ptrR + 1) > ptrR - 1 - start) {
-        quickSortHelper(array, start, ptrR - 1);
-        quickSortHelper(array, ptrR + 1, end);
-    } else {
-        quickSortHelper(array, ptrR + 1, end);
-        quickSortHelper(array, start, ptrR - 1);
-    }
-}
-
-function swap(array, i, j) {
-    [array[i], array[j]] = [array[j], array[i]];
 }
 
 export default quickSort;
