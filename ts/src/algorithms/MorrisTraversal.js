791dc51a97e7953c57acfe897cf46ab1bc9575cf
13jk59 <jesse.khaira10@gmail.com>
2021-07-18 09:58:00 -0600

    changes

--- ts/src/algorithms/MorrisTraversal.js
+++ ts/src/algorithms/MorrisTraversal.js
@@ -1,61 +1,59 @@
+import { BinaryTreeNode } from 'src/utils/binary_tree';
 /**
  *  This algorithm represents a way to traverse binary trees in-order using just
     O(1) space with the Morris Traversal algorithm.
     
-    In in-order traversal, all the nodes in a given nodes left subtree are visited,
-    then the current node is visited, then all the nodes in the right subtree of a given
-    node are visited. 
-    
-    In this case, the algorithm is being used to find the number of nodes in the binary
-    tree with just one child. 
+    In in-order traversal, all the nodes in a given nodes left subtree are
+    visited, then the current node is visited, then all the nodes in the
+    right subtree of a given node are visited.
+
+    In this case, the algorithm is being used to find the number of nodes
+    in the binary tree with just one child. 
 
     Time b/a/w: O(n) where n is the number of nodes in the tree
     Space b/a/w: O(1)
- * @param {object} node 
+ * @param {BinaryTreeNode} node 
  * @returns {Set} Set containing all nodes with one child 
  */
-export function Morris_Inorder_Traversal(node) {
+export function morrisInorderTraversal(node) {
     if (node == null) {
         return [];
     }
-    let output = new Set(); 
+    const output = new Set();
     while (node) {
         if (!node.left) {
             if (node.right) {
                 output.add(node);
             }
             node = node.right;
-        }
-        else {
-            let prevNode = getPrevNode(node);
+        } else {
+            const prevNode = getPrevNode(node);
             if (!prevNode.right) {
                 prevNode.right = node;
-                node = node.left; 
-            }
-            else {
+                node = node.left;
+            } else {
                 prevNode.right = null;
                 if (prevNode.left) {
                     output.add(prevNode);
-                }
-                else {
-                    output.delete(prevNode); 
+                } else {
+                    output.delete(prevNode);
                 }
                 if (!node.right) {
-                    output.add(node); 
+                    output.add(node);
                 }
-                node = node.right; 
+                node = node.right;
             }
         }
     }
-    return output; 
+    return output;
 }
 
 function getPrevNode(node) {
     let nodeL = node.left;
     while (nodeL.right && nodeL.right !== node) {
-        nodeL = nodeL.right; 
+        nodeL = nodeL.right;
     }
-    return nodeL; 
+    return nodeL;
 }
 
 /**
@@ -76,34 +74,31 @@ export function Morris_PreOrder_Traversal(node) {
     if (node == null) {
         return [];
     }
-    let output = new Set(); 
+    const output = new Set();
     while (node) {
         if (!node.left) {
             if (node.right) {
                 output.add(node);
             }
             node = node.right;
-        }
-        else {
-            let prevNode = getPrevNode(node);
+        } else {
+            const prevNode = getPrevNode(node);
             if (!prevNode.right) {
                 prevNode.right = node;
                 if (!node.right) {
                     output.add(node);
                 }
-                node = node.left; 
-            }
-            else {
+                node = node.left;
+            } else {
                 prevNode.right = null;
                 if (prevNode.left) {
                     output.add(prevNode);
+                } else {
+                    output.delete(prevNode);
                 }
-                else {
-                    output.delete(prevNode); 
-                }
-                node = node.right; 
+                node = node.right;
             }
         }
     }
-    return output; 
+    return output;
 }
