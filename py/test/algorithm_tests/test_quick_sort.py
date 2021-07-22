e51325abf9895695b2977c6595be072ab4f4b5c8
13jk59 <jesse.khaira10@gmail.com>
2021-07-22 17:28:18 -0600

    updated function name

--- py/test/algorithm_tests/test_quick_sort.py
+++ py/test/algorithm_tests/test_quick_sort.py
@@ -1,21 +1,21 @@
 import unittest
-from py.algorithms.quick_sort import quickSort
+from py.algorithms.quick_sort import quick_sort
 import random
 
 
 class tests(unittest.TestCase):
 
     def test1(self):
-        self.assertEqual([1, 2, 3, 4], quickSort([4, 3, 2, 1]))
+        self.assertEqual([1, 2, 3, 4], quick_sort([4, 3, 2, 1]))
 
     def test2(self):
         self.assertEqual([-300, -250, -221, 200, 2900, 30012],
-                         quickSort([30012, -221, -250, -300, 200, 2900]))
+                         quick_sort([30012, -221, -250, -300, 200, 2900]))
 
     def test3(self):
         random.seed(9)
         input_random = [random.randint(-10000, 10000) for i in range(120)]
-        self.assertEqual(sorted(input_random), quickSort(input_random))
+        self.assertEqual(sorted(input_random), quick_sort(input_random))
 
 
 if __name__ == "__main__":
