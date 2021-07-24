535445a1f4f6cf022c85e5af93207355b8693346
13jk59 <jesse.khaira10@gmail.com>
2021-07-23 19:23:34 -0600

    refactoring

--- py/algorithms/post_order_dfs_binary_tree.py.py
+++ py/algorithms/post_order_dfs_binary_tree.py.py
@@ -1,27 +1,28 @@
-
-def postOrderDFS_recursive(node, output):
-    """
-    This code describes the recursive post order traversal
+def post_order_dfs_recursive(node, output):
+    """ This code describes the recursive post order traversal
     of a binary tree. Before any given node in the tree is visited,
     all the nodes in the left subtree of the current node, and all
     the nodes in the right subtree of the current node are visited.
 
-    Inputs:
-        -> node (Binary Tree Node): Binary Tree node that is assumed 
-        to have a .left property, .right property, and .val property.
-        -> output(list)
+    Args:
+        node:
+            Object of type Binary Tree Node
+
+        output:
+            List of length zero that will store the output
+
     Returns:
-        -> list[int] containing binary trees values in post-order
+        A list of integers containing binary trees values in post-order
     """
     if not node:
         return
-    postOrderDFS_recursive(node.left,output)
-    postOrderDFS_recursive(node.right,output)
+    post_order_dfs_recursive(node.left, output)
+    post_order_dfs_recursive(node.right, output)
     output.append(node.val)
     return output
 
 
-def postOrderDFS_iterative(node):
+def post_order_dfs_iterative(node):
     """
     This code describes the iterative post order traversal
     of a binary tree. Before any given node in the tree is visited,
@@ -39,15 +40,15 @@ def postOrderDFS_iterative(node):
         return
     stack = []
     output = []
-    lastNode = None 
+    lastNode = None
     while stack or node:
         while node:
             stack.append(node)
-            node = node.left 
+            node = node.left
         if stack[-1].right and stack[-1].right != lastNode:
             node = stack[-1].right
         else:
             currNode = stack.pop()
             output.append(currNode.val)
-            lastNode = currNode 
-    return output 
+            lastNode = currNode
+    return output
