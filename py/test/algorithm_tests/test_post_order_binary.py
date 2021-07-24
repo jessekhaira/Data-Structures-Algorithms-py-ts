c6b19fb803015dcd9561ac0b055eb2deadb5cce6
13jk59 <jesse.khaira10@gmail.com>
2021-07-24 17:17:32 -0600

    added tests post order binary tree

---
+++ py/test/algorithm_tests/test_post_order_binary.py
@@ -0,0 +1,46 @@
+""" This module holds unit tests for the post order depth first
+search algorithm """
+from py.data_structures.binary_search_tree import BinarySearchTree
+from py.algorithms.post_order_dfs_binary_tree import post_order_dfs_iterative, post_order_dfs_recursive
+import unittest
+
+
+class PostOrderDFSBinary(unittest.TestCase):
+
+    def test1(self):
+        bst = BinarySearchTree(5)
+        ints = [3, 5, 1, 2, -5, -9]
+        for val in ints:
+            bst.insertion(val)
+
+        self.assertEqual(post_order_dfs_iterative(bst.root),
+                         [-9, -5, 2, 1, 3, 5, 5])
+
+    def test2(self):
+        bst = BinarySearchTree(5)
+        ints = [3, 5, 1, 2, -5, -9]
+        for val in ints:
+            bst.insertion(val)
+
+        self.assertEqual(post_order_dfs_recursive(bst.root, []),
+                         [-9, -5, 2, 1, 3, 5, 5])
+
+    def test3(self):
+        bst = BinarySearchTree(5)
+        ints = []
+        for val in ints:
+            bst.insertion(val)
+
+        self.assertEqual(post_order_dfs_iterative(bst.root), [5])
+
+    def test4(self):
+        bst = BinarySearchTree(5)
+        ints = []
+        for val in ints:
+            bst.insertion(val)
+
+        self.assertEqual(post_order_dfs_recursive(bst.root, []), [5])
+
+
+if __name__ == "__main__":
+    unittest.main()
