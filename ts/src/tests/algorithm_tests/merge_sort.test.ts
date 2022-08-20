4cb1d601073088160cfcf48b0010aa89316bd089
13jk59 <jesse.khaira10@gmail.com>
2022-08-20 13:48:17 -0600

    added new test + doc

--- ts/src/tests/algorithm_tests/merge_sort.test.ts
+++ ts/src/tests/algorithm_tests/merge_sort.test.ts
@@ -2,7 +2,7 @@ import mergesort from '../../algorithms/merge_sort';
 
 test('test 1', () => {
     const obj1 = [20, 4, 5, -9, -120];
-    expect(mergesort(obj1)).toStrictEqual([-120, -9, 4, 5, 20]);
+    expect(mergesort(obj1)).toStrictEqual(obj1.sort((a, b) => a - b));
 });
 
 test('test 2', () => {
@@ -37,4 +37,25 @@ test('test 3', () => {
         3,
     ];
     expect(mergesort(obj1)).toStrictEqual(obj1.sort((a, b) => a - b));
+});
+
+test('test 4', () => {
+    const obj = [
+        -10,
+        -9,
+        -8,
+        5,
+        2,
+        8,
+        -12321,
+        123012321,
+        1232131258,
+        1.231,
+        1.1235,
+        1238581,
+        -1231230,
+        -1232139,
+    ];
+
+    expect(mergesort(obj)).toStrictEqual(obj.sort((a, b) => a - b));
 });
