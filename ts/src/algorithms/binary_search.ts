5f0e9a340922adfefe7717c0fd40459769ac7c0a
Jesse Khaira <jesse.khaira10@gmail.com>
2022-11-26 21:05:14 -0700

    redid doc for ts binary search

--- ts/src/algorithms/binary_search.ts
+++ ts/src/algorithms/binary_search.ts
@@ -1,24 +1,25 @@
 /**
- *  This algorithm represents the binary search algorithm implemented
-    iteratively. This algorithm finds the position of a target value within
-    within a sorted array by dividing a range of values into halves, and
-    continuing to narrow down the search space until the desired value is
-    found. As such, it is an example of a divide and conquer algorithm. 
+ *  This algorithm represents the iterative low bound binary search
+    algorithm. If the target element is present in the array, the index
+    returned will represent the first time that element occurred inside the
+    array.
 
-    This algorithm assumes the simplest case of binary search
-    searching over an explicit sorted array, but the search space can
-    be any space that is sorted. For example, binary search can be used on
-    functions which are monotonically increasing or decreasing in order to find
-    the lowest possible value some condition is met (low bound binary search),
-    or the highest possible value some condition is met (high bound
-    binary search). 
-    
-    This means binary searching over the inputs of these functions to
-    determine which input produces the most optimal value -- highest or
-    lowest output value that meets some condition. 
+    This algorithm finds the position of a target value within within a sorted
+    array by dividing a range of values into halves, and continuing to narrow
+    down the search space until the desired value is found. As such, it is an
+    example of a divide and conquer algorithm.
 
-    Key tip: If the search space consists of integers, then test the binary
-    search on a two element set in order to ensure convergence. 
+    This algorithm assumes the simplest case of binary search searching over an
+    explicit sorted array, but the search space can be any space that is sorted.
+
+    For example, binary search can be used on functions which are monotonically
+    increasing  or decreasing in order to find the lowest possible value some
+    condition is met (low bound binary search), or the highest possible value
+    some condition is met (high bound binary search).
+
+    This means binary searching over the inputs of these functions to determine
+    which input produces the most optimal value (ie: the highest or lowest
+    output value that meets some condition).
  * @param {number[]} array List of integers sorted in ascending order
  * @param {number} target Integer we're looking for in the array 
  * @returns {number} Represents the idx at which the target value occurs,
